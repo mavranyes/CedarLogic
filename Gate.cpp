b8d8d4e5632017f92c1fa8f29d145d9aa7efa226
mavranyes <mavranyes@comcast.net>
2022-11-15 22:02:29 -0500

    gate .cpp and .h

--- Gate.cpp
+++ Gate.cpp
@@ -1,13 +1,100 @@
-using namespace std;
 
-#include "Gate.h"
-#include "Wire.h"
+ #include "Gate.h"
+ #include "Wire.h"
+ #include "iostream"
+ #define X 2
 
-Gate::Gate() {
+Gate::Gate (string type, int delay, Wire *in1, Wire *in2, Wire outwire) {
+    cin >> type;
+    type = stoi(type); 
+    cin >> delay;
+    in1 = ;//Need to connect to the pointer
+    in2 = ;//Need to connect to the pointer
+    cin >> outwire;//Need to connect to the pointer
+}
 
+//Gate::Gate (GateType type, int delay, Wire *in1, Wire *in2, Wire *outwire) {}
 
+void evaluate(){
+    int a=in1->getValue();
+    int b = -1;
+    int out = -1;
+    if (in2 != nullptr) {
+        b=in2->getValue();
+    }
+    switch(type){
+        case AND:
+            out = And(a, b);
+            break;
+        case NOT:
+            out = Not(a);
+            break;
+        case OR:
+            out = Or(a, b);
+            break;
+        case XNOR:
+            out = Xnor(a, b);
+            break;
+        case XOR:
+            out = Xor(a, b);
+            break;
+        case NAND:
+            out = Nand(a, b);
+            break;
+        case NOR:
+            out = Nor(a, b);
+            break;
+    }
+    outwire->setValue(out);
+}
+
+int Gate::And(int a, int b){
+    if (a==0||b==0){
+        return 0;
+    }
+    else if(a==X||b==X){
+        return X;
+    }
+    return 1;
+}
+
+int Gate::Or(int a, int b){
+    if (a==1||b==1){
+        return 1;
+    }
+    else if(a==X||b==X){
+        return X;
+    }
+    return 0;
+}
+
+int Gate::Not(int a){
+    if(a==X){
+        return X;
+    }
+    else if (a==0){
+        return 1;
+    }
+    return 0;
+}
+
+int Gate::Xor(int a, int b) {
+    return Or(And(Not(a), b), And(a, Not(b)));
+}
+
+int Gate::Nand(int a, int b){
+    return Not(And(a, b));
+}
+
+int Gate::Xnor(int a, int b){
+    return Not(Xor(a, b));
+}
+
+int Gate::Nor(int a, int b){
+    return Not(Or(a, b));
 }
 
 int main() {
+
 
 };
--- Gate.h
+++ Gate.h
@@ -1,23 +1,31 @@
-//Code by Micah Vranyes and Alben Augestine
-using namespace std;
 
-#include <string>
+ #include <string>
 
-class Wire;
+ enum GateType {NOT, AND, OR, XOR, NAND, NOR, XNOR};
 
-class Gate {
-    public:
-        string type;
-        int delay;
-        Wire *in1, *in2;
-        Wire *outwire;
+ class Wire;
 
-        Gate::Gate (string type, int delay, Wire *in1, Wire in2, Wire outwire);
-        int getDelay()const;
-        Wire* getInput(int num)const;
-        Wire* getOutput()const;
-        string evaluate()const;//Runs gate logic
+ class Gate {
+     public:
+         string type;
+         GateType type;
+         int delay;
+         Wire *in1, *in2;
+         Wire *outwire;
 
+         Gate::Gate (string type, int delay, Wire *in1, Wire *in2, Wire outwire);
+         //Gate::Gate (GateType type, int delay, Wire *in1, Wire *in2, Wire *outwire);
+         int getDelay()const;
+         Wire* getInput(int num)const;
+         Wire* getOutput()const;
+         void evaluate()const;//Runs gate logic
 
-
-};
+     private:
+         int And(int, int);
+         int Or(int, int);
+         int Not(int);
+         int Xor(int, int);
+         int Nand(int, int);
+         int Xnor(int, int);
+         int Nor(int, int);
+ };
